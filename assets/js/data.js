727303482965c4f34fe61ced6d82763bd5a9c026
AlmostDebugged <luka.predojevic@outlook.com>
2022-10-28 08:42:12 +0000

    add Setter and Getter methods for DataController

--- assets/js/data.js
+++ assets/js/data.js
@@ -45,6 +45,36 @@ class DataController {
     }
 
     /**
+     * Setter for editing user data.
+     * @param {String}   key    Key you want to edit.
+     * @param {*}        value  Value you want to set.
+     * @return {Boolean} 
+     */
+    setValue(key, value) {
+
+        if (typeof(key) != "string") return false
+
+        if (key in this.registeredElementSyncs) {
+
+            this.registeredElementSyncs[key].innerHTML = value
+
+        }
+
+    }
+
+    /**
+     * Getter for retrieving user data.
+     * @param {String}   key  Key of which you want to get the value.
+     * @return {*}            Value you retrieved using given key.
+     */
+     getValue(key) {
+
+        if (typeof(key) != "string") return false
+        return this.data[key]
+
+    }
+
+    /**
     * Method attempts to mirgrate users data from a old schema to a new schema. 
     * @param  {Object} oldData  User data using a old schema.
     * @return {Object}          Migrated data using new schema.
@@ -72,8 +102,9 @@ class DataController {
 
     /**
     * This method allows you to sync a html elements value with a value from data.
-    * @param {Object} domElement  The element you wish to sync.
-    * @param {String} dataKey     The key for the value you want to be sync from data.
+    * Whenever a synced value is changed using the setValue method the given Elements innerHTML will be set to the new value.
+    * @param {Object}  domElement  The element you wish to sync.
+    * @param {String}  dataKey     The key for the value you want to be sync from data.
     * @return {Boolean}           Indicates succesion of 
     */
     registerElementSync(domElement, dataKey) {
