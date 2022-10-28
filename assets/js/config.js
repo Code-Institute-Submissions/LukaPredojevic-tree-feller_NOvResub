6cfb18346f83218ed03fb016082a6ba5739aeeb9
AlmostDebugged <luka.predojevic@outlook.com>
2022-10-28 09:03:34 +0000

    update config.js DEFAULT_SCHEMA

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,20 @@
 export default {
 
     "DEFAULT_SCHEMA": {
-        "version": 0,
-        "first_time_login": Date.now()
+
+        version: 0,
+
+        first_time_login: Date.now(),
+        ingameTime: 0,
+
+        woodAmount: 0,
+        totalWood: 0,
+
+        upgrades: {
+            axes: 0,
+            workers: 0
+        }
+
     }
 
 }
